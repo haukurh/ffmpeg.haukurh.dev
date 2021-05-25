01840db3868037dec602af72d13fce665f8e4de6
Haukur Hlöðversson <haukur@haukurh.is>
2021-05-25 18:46:12 +0000

    CloudFront invalidation build, handle index.html files

--- build/createCloudfrontInvalidation.sh
+++ build/createCloudfrontInvalidation.sh
@@ -12,10 +12,12 @@ if [ ! -f $TMP_FILE ]; then
   exit 2
 fi
 
-SYNCED_FILES=$(cat synced-files.tmp)
-FILES_JSON=$(cat $TMP_FILE | jq -Rs 'split("\n")' | jq -rc '[.[] | select(length > 0) | gsub("^\\s+|\\s+$";"")]')
-FILES_LENGTH=$(cat $TMP_FILE| jq -Rs 'split("\n")' | jq -rc '[.[] | select(length > 0) | gsub("^\\s+|\\s+$";"")] | length')
+# Account for /index.html files as well, (e.g. /index.html => /)
+grep -E "index.html$" $TMP_FILE | sed -E "s/index.html//" >> $TMP_FILE
+
+FILES_JSON=$(cat $TMP_FILE | jq -Rs 'split("\n")' | jq -rcS '[.[] | select(length > 0) | gsub("^\\s+|\\s+$";"")]')
+FILES_LENGTH=$(cat $TMP_FILE| jq -Rs 'split("\n")' | jq -rcS '[.[] | select(length > 0) | gsub("^\\s+|\\s+$";"")] | length')
 
 JSON="{\"Paths\": {\"Quantity\": $FILES_LENGTH,\"Items\": $FILES_JSON},\"CallerReference\": \"github-action-$GITHUB_RUN_ID-$GITHUB_RUN_NUMBER\"}"
 
-echo $JSON >> files.json
+echo $JSON > files.json
